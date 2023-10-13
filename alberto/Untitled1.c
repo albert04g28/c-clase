0eadeb026f175263e45b6ce39aaeed3ce00aaa20
hola <apariciogallo@gmail.com>
2023-10-13 20:12:57 +0200

    hola

---
+++ alberto/Untitled1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {     /* Un programa sencillo */
+
+    int num;
+
+    num = 1;
+
+    printf("Maravilla, colega!\n");
+    printf("Mi numero es el %d por ser el primero.\n\n", num);
+
+    system("pause");
+    return 0;
+}
---
+++ alberto/hola.h
@@ -0,0 +1,20 @@
+#ifndef HOLA_H_INCLUDED
+#define HOLA_H_INCLUDED
+
+
+
+#endif // HOLA_H_INCLUDED
+#include <stdio.h>
+
+int main() {     /* Un programa sencillo */
+
+    int num;
+
+    num = 1;
+
+    printf("Maravilla, colega!\n");
+    printf("Mi numero es el %d por ser el primero.\n\n", num);
+
+    system("pause");
+    return 0;
+}
